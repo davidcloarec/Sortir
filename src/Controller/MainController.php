9051580d5ef1ae0f267542823292ab352be043de
Flegoff2318 <96115160+Flegoff2318@users.noreply.github.com>
2023-06-26 11:44:17 +0200

    User + registration form Entity User modified with username (required for account registration, not required for authentication)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
+    #[IsGranted("ROLE_USER")]
     #[Route('/main', name: 'app_main')]
     public function index(): Response
     {
