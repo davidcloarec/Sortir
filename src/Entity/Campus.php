0d6d5b1e3fd787bece7ea331faa81257d50bc0f3
davidcloarec <david.cloarec2023@campus-eni.fr>
2023-06-27 09:08:38 +0200

    create activity

--- src/Entity/Campus.php
+++ src/Entity/Campus.php
@@ -2,12 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\SiteRepository;
+use App\Repository\CampusRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: SiteRepository::class)]
+#[ORM\Entity(repositoryClass: CampusRepository::class)]
 class Campus
 {
     #[ORM\Id]
